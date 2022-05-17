1284dfe15443de0b304691755fe2163e7113d594
danielkohlstrunk <danielko86@web.de>
2022-05-17 13:08:35 +0200

    React lernen

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <h1>Hello World!Wünsche euch einen schönen sonnigen Tag!</h1>
     </div>
   );
 }
